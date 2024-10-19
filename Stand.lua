20bb5e9d4e93f33f609b5a0c31eaa1472de6ddba
AOC <codex.lol.test.new@gmail.com>
2024-10-19 15:42:45 +0700

    Stand.lua

---
+++ Stand.lua
@@ -0,0 +1,36 @@
+getgenv()._=
+
+    'This script made by "k.6736/OSGC" | https://discord.gg/VgT5Suy3Bf'
+
+getgenv().LynixSettings = {
+    --// ! Main ! \\--
+    ['Owner'] = 'ice146782',
+    ['Position'] = 'Back', --Position Of Stand [Back | Left | Right]
+    ['CrewID'] = 2, --Must Be In Group For It To Work
+    ['Attack'] = 'Heavy', --Heavy = Super Punch Light = Spam Punch [Heavy | Light]
+    ['StandMode'] = 'D4C', --[Star Platinum : OverHeaven | Star Platinum: The World | Star Platinum, Za Warudo! | TheWorld | Cmoon | King Crimson | Killer Queen | MIH | D4C]
+    ['FlyMode'] = 'Glide', --[Glide | Fly | Hover | Heaven | Swim]
+    ['Prediction'] = 0.3, --Prediction For Attack Or Targetting
+    ['GunPrediction'] = 0.037, --Prediction For Gun Kill
+    ['AttackMode'] = 'Sky', --[Sky | Under] If [Sky] The Stand Will Attack Infront Of Then And If [Under] The Stand Will Attack In Underground
+    ['GunMode'] = 'Under', --[Sky | Under]  The Stand Will Shooting Just Like What In The Settings
+    ['Resolver'] = false, --[true | false] Turn On Or Off Resolver If Someone Using Anti Lock
+    --// ! Misc ! \\--
+    ['CustomSummon'] = 's', --Custom [Summon!] Text
+    ['LowGraphics'] = true, --[true | false] Turns graphics down so your stand doesnt lag too much
+    ['AntiStomp'] = true, --[true | false] The Stand Will Cannot Be Stomped Or Grabbed
+    ['CustomName'] = 'OSGC', --Custom Name Text Default Is [Master] Example Of it 'Im Here, Master!'
+    ['Hidescreen'] = false, --[true | false] If [true] You WIll See White Blank Screen But The Performance Is Gonna Be Good]
+    ['ChatCmds'] = true, --[true | false] Chatted When You Use Some Command
+    ['MaskMode'] = 'Ninja', --[Surgeon | Paintball | Skull | Pumpkin | Hockey | Ninja | Riot] The Stand Will Buy The Mask You Selected
+    ['AutoMask'] = true, --[true | false] Auto Buy Mask When The Stand Respawn
+    ['60'] = true, --[false | 'NUMBER'] This is so your main account gets the most fps (MAXED IS 60, SUGGEST 30 FPS ON STANDS)
+    ['Msg'] = 'AOC ON TOP.', --When You Say [/e msg] It Makes All The Alts Say That Message
+    ['Alt'] = false,
+    --//    ! Sounds !     \\--
+    --\\ MUST HAVE BOOMBOX //--
+    ['Sounds'] = true,
+    ['SummonMusic'] = true, --[true | false]
+    ['CustomSong'] = 1839246711 --Enter A Song ID And Say 'Song!'
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AOCScript/Stand/main/Protected_9363038714740400.lua"))()
